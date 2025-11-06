7b6d7dde970501f0e9bd2c31d832a37754aaa728
Manuel Gil <manuelgilsitio@gmail.com>
2025-11-06 07:58:28 -0500

    Updating models to make some fields optional

--- src/yourag/youtube/models.py
+++ src/yourag/youtube/models.py
@@ -6,13 +6,13 @@ from datetime import timedelta
 class TranscriptEntry(BaseModel):
     text: str
     start: timedelta
-    end: timedelta
+    end: Optional[timedelta] = None
     duration: timedelta
 
 
 class Transcript(BaseModel):
     video_id: str
-    language: str
+    language: Optional[str] = None
     entries: List[TranscriptEntry] = Field(default_factory=list)
 
 
