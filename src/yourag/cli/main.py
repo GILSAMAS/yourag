a82f813ab76905d05d2c0c1ba096008c2af1f895
Manuel Gil <manuelgilsitio@gmail.com>
2025-11-07 23:09:44 -0500

    adding store subcommand

--- src/yourag/cli/main.py
+++ src/yourag/cli/main.py
@@ -8,26 +8,44 @@ def cli():
     ingest_parser = subparsers.add_parser("ingest", help="Ingest a YouTube video")
     ingest_parser.add_argument(
         "-v",
-        "--video_id",
+        "--video-id",
         required=True,
         type=str,
         help="The ID of the YouTube video to ingest",
+    )
+    ingest_parser.add_argument(
+        "-n",
+        "--name",
+        required=False,
+        type=str,
+        help="The name to assign to the video collection",
+        default=ingest_parser.get_default("video-id"),
     )
 
     query_parser = subparsers.add_parser("query", help="Query a YouTube video")
     query_parser.add_argument(
         "-v",
-        "--video_id",
+        "--video-id",
         required=True,
         type=str,
         help="The ID of the YouTube video to query",
     )
     query_parser.add_argument(
         "-q",
-        "--query_text",
+        "--query-text",
         required=True,
         type=str,
         help="The text query to search within the video content",
+    )
+
+    store_parser = subparsers.add_parser("store", help="Manage the vector store")
+    # Additional subcommands for store can be added here
+    store_parser.add_argument(
+        "-ls",
+        "--list",
+        action="store_true",
+        required=True,
+        help="List all collections in the vector store",
     )
 
     args = parser.parse_args()
@@ -35,9 +53,14 @@ def cli():
     if args.command == "ingest":
         from yourag.cli.commands.ingest import ingest_video
 
-        ingest_video(args.video_id)
+        ingest_video(args.video_id, args.name)
 
     elif args.command == "query":
         from yourag.cli.commands.query import query_video
 
         query_video(args.video_id, args.query_text)
+
+    elif args.command == "store":
+        from yourag.cli.commands.store import list_collections
+
+        list_collections()
